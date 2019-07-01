d5f12ac694d3512b6930a8966a45cab69a9511e6
David Mullins <Mullins6323@gmail.com>
2019-07-01 16:08:07 -0500

    auth working

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,52 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app';
 
-import '../index';
+import Auth from '../components/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Home from '../components/Home/Home';
 
-function App() {
-  return (
+import fbConnection from '../helpers/data/connection';
+
+import './App.scss';
+
+fbConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return (
+        <Home />
+        );
+      }
+      return <Auth />;
+    };
+    return (
     <div className="App">
-      <p>
-      <button type="button" className="btn btn-danger">Danga</button>
-      </p>
+      <MyNavbar authed={authed} />
+      {loadComponent()}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
